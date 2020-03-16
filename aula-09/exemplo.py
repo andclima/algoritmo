c0d458131d9bfed47b23b119d81cfc2e0f15ce5c
Anderson Lima <andclima@gmail.com>
2020-03-16 12:19:27 -0300

    v1.0

---
+++ aula-09/exemplo.py
@@ -0,0 +1,11 @@
+# Alô, som!
+nome = "João"
+idade = 21
+valor = 29.31
+
+print(f"{nome} tem {idade} anos e possui R$ {valor} na carteira")
+if idade < 21:
+    print(f"{nome} ainda não possui idade suficiente para maioridade civil.")
+else:
+    print(f"{nome} já pode ser declarado civilmente capaz.")
+print("Até a próxima!")
