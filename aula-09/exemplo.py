fa387fefa69376a8073c7ec08553b9a41c434860
Anderson Lima <andclima@gmail.com>
2020-03-17 23:15:22 -0300

    v1.2

--- aula-09/exemplo.py
+++ aula-09/exemplo.py
@@ -1,11 +1,13 @@
 # Alô, som!
-nome = "João"
+nome = "Joao"
 idade = 21
 valor = 29.31
 
+idade = int(input(f"Informe a idade de {nome}: "))
+
 print(f"{nome} tem {idade} anos e possui R$ {valor} na carteira")
 if idade < 21:
-    print(f"{nome} ainda não possui idade suficiente para maioridade civil.")
+    print(f"{nome} ainda nao possui idade suficiente para maioridade civil.")
 else:
-    print(f"{nome} já pode ser declarado civilmente capaz.")
-print("Até a próxima!")
+    print(f"{nome} ja pode ser declarado civilmente capaz.")
+print("Ate a proxima!")
